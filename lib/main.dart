ef0ebefa340a3f0e3a13ce70c3075f5535362a24
Anshul M <anshmaske743@gmail.com>
2021-09-27 12:43:12 -0400

    Added listview avoid page overflow

--- lib/main.dart
+++ lib/main.dart
@@ -63,112 +63,116 @@ class _LoginPageState extends State<LoginPage> {
         // title: Text(widget.title),
         automaticallyImplyLeading: true,
       ),
-      body: Column(
-        // crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const SizedBox(
-            height: 50,
-          ),
-          Padding(
-            padding: EdgeInsets.all(20),
-            child: TextFormField(
-              autovalidate: false,
-              key: formkey,
-              decoration: const InputDecoration(
-                labelText: 'EMAIL',
-                hintStyle: TextStyle(fontSize: 10),
-                hintText: 'Enter Email',
-                border: OutlineInputBorder(),
-                suffixIcon: Padding(
-                  padding: EdgeInsets.only(bottom: 12.0),
-                  child: Icon(
-                    Icons.email_rounded,
+      body: ListView(children: [
+        Column(
+          // crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(
+              height: 50,
+            ),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: TextFormField(
+                autovalidate: false,
+                key: formkey,
+                decoration: const InputDecoration(
+                  labelText: 'EMAIL',
+                  hintStyle: TextStyle(fontSize: 10),
+                  hintText: 'Enter Email',
+                  border: OutlineInputBorder(),
+                  suffixIcon: Padding(
+                    padding: EdgeInsets.only(bottom: 12.0),
+                    child: Icon(
+                      Icons.email_rounded,
+                    ),
                   ),
                 ),
-              ),
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return 'Email Required';
-                } else {
-                  return null;
-                }
-              },
-              // validator: validatepass,
-            ),
-          ),
-          new SizedBox(height: 50),
-          Padding(
-            padding: EdgeInsets.all(20),
-            child: TextFormField(
-              autovalidate: false,
-              decoration: InputDecoration(
-                labelText: 'PASSWORD',
-                hintStyle: TextStyle(fontSize: 10),
-                hintText: 'Enter Password',
-                border: OutlineInputBorder(),
-                suffixIcon: InkWell(
-                  onTap: _togglePasswordView,
-                  child: Icon(
-                    _isHidden ? Icons.visibility : Icons.visibility_off,
-                  ),
-                ),
-                // onTap: _togglePasswordView,
-              ),
-              obscureText: _isHidden,
-              // suffix: Icon(Icons.visibility),
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return 'Password Required';
-                } else {
-                  return null;
-                }
-              },
-            ),
-          ),
-          new SizedBox(height: 5),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => NextScreen()));
-            },
-            child: Text('SIGN IN'),
-            style: ButtonStyle(
-              padding: MaterialStateProperty.all<EdgeInsets>(
-                  EdgeInsets.fromLTRB(30, 15, 30, 15)),
-            ),
-          ),
-          new SizedBox(height: 15),
-          Container(
-            padding: EdgeInsets.symmetric(),
-            child: InkWell(
-              child: Text('FORGOT PASSWORD'),
-              onTap: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => ResetPassword()));
-              },
-            ),
-            color: Colors.blueGrey[50],
-          ),
-          SizedBox(height: 200),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'New to Practise App?',
-              ),
-              FlatButton(
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => CreateAccount()));
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Email Required';
+                  } else {
+                    return null;
+                  }
                 },
-                child: Text('Signup'),
-                textColor: Colors.blue,
+                // validator: validatepass,
               ),
-              new SizedBox(height: 5),
-            ],
-          ),
-        ],
-      ),
+            ),
+            new SizedBox(height: 50),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: TextFormField(
+                autovalidate: false,
+                decoration: InputDecoration(
+                  labelText: 'PASSWORD',
+                  hintStyle: TextStyle(fontSize: 10),
+                  hintText: 'Enter Password',
+                  border: OutlineInputBorder(),
+                  suffixIcon: InkWell(
+                    onTap: _togglePasswordView,
+                    child: Icon(
+                      _isHidden ? Icons.visibility : Icons.visibility_off,
+                    ),
+                  ),
+                  // onTap: _togglePasswordView,
+                ),
+                obscureText: _isHidden,
+                // suffix: Icon(Icons.visibility),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Password Required';
+                  } else {
+                    return null;
+                  }
+                },
+              ),
+            ),
+            new SizedBox(height: 5),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => NextScreen()));
+              },
+              child: Text('SIGN IN'),
+              style: ButtonStyle(
+                padding: MaterialStateProperty.all<EdgeInsets>(
+                    EdgeInsets.fromLTRB(30, 15, 30, 15)),
+              ),
+            ),
+            new SizedBox(height: 15),
+            Container(
+              padding: EdgeInsets.symmetric(),
+              child: InkWell(
+                child: Text('FORGOT PASSWORD'),
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => ResetPassword()));
+                },
+              ),
+              color: Colors.blueGrey[50],
+            ),
+            SizedBox(height: 200),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'New to Practise App?',
+                ),
+                FlatButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => CreateAccount()));
+                  },
+                  child: Text('Signup'),
+                  textColor: Colors.blue,
+                ),
+                new SizedBox(height: 5),
+              ],
+            ),
+          ],
+        ),
+      ]),
     );
   }
 
